79fbf7b6eb58d03499ae402f41b53bb5bb046a8f
Marcus Müller <znek@mulle-kybernetik.com>
2009-01-07 15:26:19 +0000

    v1.1.7b7, removed Tiger build and changed copyright notices

--- FUSEOFS/FUSEOFSFileProxy.m
+++ FUSEOFS/FUSEOFSFileProxy.m
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2007-2008, Marcus Müller <znek@mulle-kybernetik.com>.
+  Copyright (c) 2007-2009, Marcus Müller <znek@mulle-kybernetik.com>.
   All rights reserved.
 
 
--- iTunesPlaylist.m
+++ iTunesPlaylist.m
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2007-2008, Marcus Müller <znek@mulle-kybernetik.com>.
+  Copyright (c) 2007-2009, Marcus MÃ¼ller <znek@mulle-kybernetik.com>.
   All rights reserved.
 
 
